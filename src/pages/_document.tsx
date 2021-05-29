8406046dcd65ec84668d9e96281a59c0a5bbe982
nntakuya <uv.nkmt.yii@gmail.com>
2021-05-29 17:33:45 +0900

    first commit

---
+++ src/pages/_document.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+interface MyDocumentInterface {
+  url: string
+  title: string
+  description: string
+}
+
+class MyDocument extends Document implements MyDocumentInterface {
+  url = 'https://example.com'
+  title = 'Demo'
+  description = 'test text'
+
+  render(): JSX.Element {
+    return (
+      <Html lang="ja-JP">
+        <Head>
+          {/* コメント */}
+          <meta name="description" content={this.description} />
+          <meta name="theme-color" content="#333" />
+          <meta property="og:title" content={this.title} />
+          <meta property="og:description" content={this.description} />
+          <meta property="og:site_name" content={this.title} />
+          <meta property="og:image" content={`${this.url}/ogp.png`} />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
