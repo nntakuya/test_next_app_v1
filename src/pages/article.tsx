777703f6bf056aebb0faec68d063c15eb3b102c4
nntakuya <uv.nkmt.yii@gmail.com>
2021-06-06 19:55:42 +0900

    コンポーネントにpropsを追加

--- src/pages/article.tsx
+++ src/pages/article.tsx
@@ -1,7 +1,38 @@
-import { NextPage } from 'next'
+import Article from '../components/Article'
 
-const Article: NextPage = () => {
-  return <p>test</p>
+export default function ArticleList({ allArticles }) {
+  return allArticles.map((article, index) => {
+    return (
+      <Article
+        key={index}
+        title={article.title}
+        description={article.description}
+        purpose_category={article.purpose_category}
+        tag={article.tag}
+      />
+    )
+  })
 }
 
-export default Article
+export async function getStaticProps() {
+  const allArticles = [
+    {
+      title: 'sample text',
+      description: 'sample text',
+      purpose_category: 'sample text',
+      tag: 1,
+    },
+    {
+      title: 'sample text2',
+      description: 'sample text2',
+      purpose_category: 'sample text2',
+      tag: 2,
+    },
+  ]
+
+  return {
+    props: {
+      allArticles,
+    },
+  }
+}
