e6166341a72b2740af41d40bbf1bd5b7136f2002
nntakuya <uv.nkmt.yii@gmail.com>
2021-06-06 15:58:37 +0900

    保存先をLocal Storageへ指定

--- src/pages/article/new.tsx
+++ src/pages/article/new.tsx
@@ -1,8 +1,10 @@
 import { useForm, SubmitHandler } from 'react-hook-form'
 
 type Inputs = {
-  example: string
-  exampleRequired: string
+  title: string
+  description: string
+  purpose_category: string
+  tag: number
 }
 
 export default function App() {
@@ -12,9 +14,17 @@ export default function App() {
     watch,
     formState: { errors },
   } = useForm<Inputs>()
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
 
-  console.log(watch('example'))
+  // [保存先] LocalStorage
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    const items = JSON.parse(localStorage.getItem('test'))
+    const list = [...items]
+
+    list.push(data)
+    localStorage.setItem('test', JSON.stringify(list))
+  }
+
+  //console.log(watch('title'))
 
   return (
     <div className="grid grid-cols-1 gap-6 m-16">
@@ -22,22 +32,85 @@ export default function App() {
         <div className="flex flex-wrap -mx-3 mb-6">
           <div className="w-full px-3">
             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-              タイトル
+              Title
             </label>
             <input
               className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              defaultValue="test"
-              {...register('example', { required: true })}
+              {...register('title', { required: true })}
             />
             <p className="text-red-500 text-xs italic">
-              {errors.example && <p>This field is required</p>}
+              {errors.title && <p>This field is required</p>}
             </p>
           </div>
         </div>
-        <input
-          className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-          type="submit"
-        />
+
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
+            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+              Description
+            </label>
+            <textarea
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              {...register('description', { required: true })}
+            />
+            <p className="text-red-500 text-xs italic">
+              {errors.description && <p>This field is required</p>}
+            </p>
+          </div>
+        </div>
+
+        <div className="inline-block relative w-64 mb-6">
+          <label className="inline-block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+            Purpose
+          </label>
+          <select
+            className="inline-block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+            {...register('purpose_category')}
+          >
+            <option value="study">study</option>
+            <option value="work">work</option>
+            <option value="hobby">hobby</option>
+          </select>
+          <div className="pointer-events-none absolute top-10 right-0 flex items-center px-2 text-gray-700">
+            <svg
+              className="fill-current h-4 w-4"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+            </svg>
+          </div>
+        </div>
+
+        <div className="inline-block relative w-64 mb-6">
+          <label className="inline-block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+            Tag
+          </label>
+          <select
+            className="inline-block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+            {...register('tag')}
+          >
+            <option value="1">Tag1</option>
+            <option value="2">Tag2</option>
+            <option value="3">Tag3</option>
+          </select>
+          <div className="pointer-events-none absolute top-10 right-0 flex items-center px-2 text-gray-700">
+            <svg
+              className="fill-current h-4 w-4"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+            </svg>
+          </div>
+        </div>
+
+        <div className="block relative w-64">
+          <input
+            className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+            type="submit"
+          />
+        </div>
       </form>
     </div>
   )
